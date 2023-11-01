48490a80229e496eacae370594b0f89c9699c41a
Olena Shliegel <olenashliegel@gmail.com>
2023-11-01 10:25:39 -0700

    It will add fucntionality for eventListener

--- js/newscripts.js
+++ js/newscripts.js
@@ -1,7 +1,5 @@
-// UI Logic
-
 function getAndSetMadLibValues() {
-  console.log("Submit form successfully reached.");
+  // in this section we get the value for each form input
   const person1Input = document.getElementById("person1Input").value;
   const person2Input = document.getElementById("person2Input").value;
   const animalInput = document.getElementById("animalInput").value;
@@ -9,6 +7,7 @@ function getAndSetMadLibValues() {
   const verbInput = document.getElementById("verbInput").value;
   const nounInput = document.getElementById("nounInput").value;
 
+  // then we set the story variables to the values we got from the form
   document.querySelector("span#person1a").innerText = person1Input;
   document.querySelector("span#person1b").innerText = person1Input;
   document.querySelector("span#person1c").innerText = person1Input;
@@ -18,23 +17,40 @@ function getAndSetMadLibValues() {
   document.querySelector("span#verb").innerText = verbInput;
   document.querySelector("span#noun").innerText = nounInput;
   document.querySelector("span#exclamation").innerText = exclamationInput;
+
 }
 
-// this function creates the onsubmit event handler
-// it needs to be called in the window.onload event handler
-function setFormSubmissionEventHandler() {
+function advertisement() {
+  window.alert("Do you need a new computer? Visit www.superextracomputersales.com to find the best deals!");
   let form = document.querySelector("form");
-  form.onsubmit = function (event) {
-    event.preventDefault();
-    getAndSetMadLibValues();
-    document.querySelector("div#story").removeAttribute("class");
-  }
+  form.removeEventListener("submit", advertisement);
 }
 
 window.onload = function () {
-  console.log("Script executing!");
-  // inside of the window.onload handler function we only include
-  // the code that we want to run when
-  // the webpage has finished loading all resources
-  setFormSubmissionEventHandler();
+  // first we set up an event handler for the form submission
+  let form = document.querySelector("form");
+  let resetBtn = document.querySelector("button#reset");
+  let story = document.querySelector("div#story");
+
+  form.addEventListener("submit", function (event) {
+    getAndSetMadLibValues();
+    story.removeAttribute("class");
+    event.preventDefault();
+  });
+
+  form.addEventListener("submit", function () {
+    resetBtn.removeAttribute("class");
+  });
+
+  form.addEventListener("submit", advertisement);
+
+  resetBtn.addEventListener("click", function () {
+    story.setAttribute("class", "hidden");
+    document.getElementById("person1Input").value = null;
+    document.getElementById("person2Input").value = null;
+    document.getElementById("animalInput").value = null;
+    document.getElementById("exclamationInput").value = null;
+    document.getElementById("verbInput").value = null;
+    document.getElementById("nounInput").value = null;
+  });
 };
